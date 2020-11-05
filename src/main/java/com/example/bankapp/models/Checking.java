1f5f63c829a38c34b7b44ee2c57a6cfc4ba2c1c9
Shane Kaestner <shane.kaestner@yahoo.com>
2020-11-05 13:33:14 -0800

    updated checking and loan classes.

--- src/main/java/com/example/bankapp/models/Checking.java
+++ src/main/java/com/example/bankapp/models/Checking.java
@@ -1,6 +1,53 @@
 package com.example.bankapp.models;
 
+import java.util.UUID;
+
 public class Checking {
 
+    private UUID id;
 
+    private double balance; // 8 bytes can contain decimal
+
+    private double interest;
+
+    public Checking(
+        UUID id,
+        double balance,
+        double interest) {
+        this.id = id;
+        this.balance = balance;
+        this.interest = 1.5;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void addToBalance(double amount) {
+        this.balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void increaseInterest(double amount) {
+        if (amount <= 1 && amount >= -1) {
+            this.interest += amount;
+        } else {
+            System.out.println("That is an invalid amount.");
+        }
+    }
+    public double getInterest() {
+        return interest;
+    }
+
+    @Override
+    public String toString() {
+        return "Savings{" +
+            "id=" + id +
+            ", balance=" + balance +
+            ", interest=" + interest +
+            '}';
+    }
 }
